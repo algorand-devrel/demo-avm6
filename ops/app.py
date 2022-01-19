afa5494d75d9644bbcd00d2403f321ceb329dce4
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-01-19 15:39:11 -0500

    adding first pass with new ops

---
+++ ops/app.py
@@ -0,0 +1,101 @@
+import os
+from pyteal import *
+from pytealutils.strings import itoa
+
+# This may be provided as a constant in pyteal, for now just hardcode
+prefix = Bytes("base16", "151f7c75")
+
+acct_param_selector = MethodSignature("acct_param(account)string")
+budget_pad_selector = MethodSignature("pad()void")
+
+@Subroutine(TealType.none)
+def doit():
+
+    # gitxn t f	        : field F of the Tth transaction in the last inner group submitted
+    # gitxna t f i      : Ith value of the array field F from the Tth transaction in the last inner group submitted
+    # gloadss	        : Bth scratch space value of the Ath transaction in the current group
+
+    pass
+
+
+@Subroutine(TealType.none)
+def acct_param():
+    # acct_params_get   : get AcctBalance, AcctMinBalance, AcctAuthAddr for a given account
+    acct_ref = Btoi(Txn.application_args[1])
+    return ret_log(
+        Seq(
+            aa := AcctParam.authAddr(acct_ref),
+            mb := AcctParam.minBalance(acct_ref),
+            b := AcctParam.balance(acct_ref),
+            Concat(
+                Bytes("Auth addr: '"),
+                If(aa.hasValue() , aa.value(), Bytes("<None>")),
+                Bytes("', Min balance: "),
+                itoa(If(mb.hasValue(), mb.value(), Int(0))),
+                Bytes(", Balance: "),
+                itoa(If(b.hasValue(), b.value(), Int(0))),
+            )
+        )
+    )
+
+#@Subroutine(TealType.none)
+#def bsqrt():
+#    # bsqrt             : The largest integer I such that I^2 <= A. A and I are interpreted as big-endian unsigned integers
+#
+#    pass
+
+# Util to add length to string to make it abi compliant, will have better interface in pyteal
+@Subroutine(TealType.bytes)
+def string_encode(str: TealType.bytes):
+    return Concat(Extract(Itob(Len(str)), Int(6), Int(2)), str)
+
+# Util to log bytes with return prefix
+@Subroutine(TealType.none)
+def ret_log(value: TealType.bytes):
+    return Log(Concat(prefix, string_encode(value)))
+
+
+
+def approval():
+    # Define our abi handlers, route based on method selector defined above
+    handlers= [
+        [
+            Txn.application_args[0] == acct_param_selector,
+            Return(Seq(acct_param(), Int(1))),
+        ],
+        [
+            Txn.application_args[0] == budget_pad_selector,
+            Approve(),
+        ]
+    ]
+
+    return Cond(
+        [Txn.application_id() == Int(0), Approve()],
+        [Txn.on_completion() == OnComplete.DeleteApplication, Return(Txn.sender() == Global.creator_address())],
+        [Txn.on_completion() == OnComplete.UpdateApplication, Return(Txn.sender() == Global.creator_address())],
+        [Txn.on_completion() == OnComplete.CloseOut, Approve()],
+        [Txn.on_completion() == OnComplete.OptIn, Approve()],
+        *handlers,
+    )
+
+
+def clear():
+    return Return(Int(1))
+
+
+def get_approval():
+    return compileTeal(approval(), mode=Mode.Application, version=6)
+
+
+def get_clear():
+    return compileTeal(clear(), mode=Mode.Application, version=5)
+
+
+if __name__ == "__main__":
+    path = os.path.dirname(os.path.abspath(__file__))
+
+    with open(os.path.join(path, "approval.teal"), "w") as f:
+        f.write(get_approval())
+
+    with open(os.path.join(path, "clear.teal"), "w") as f:
+        f.write(get_clear())
---
+++ ops/demo.py
@@ -0,0 +1,108 @@
+from algosdk import *
+from algosdk.atomic_transaction_composer import *
+from algosdk.abi import *
+from algosdk.v2client import algod
+from algosdk.future.transaction import *
+from sandbox import get_accounts
+import base64
+import os
+
+from app import get_approval, get_clear
+
+client = algod.AlgodClient("a"*64, "http://localhost:4001")
+
+def get_method(c: Contract, name: str) -> Method:
+    for m in c.methods:
+        if m.name == name:
+            return m
+    raise Exception("No method with the name {}".format(name))
+
+
+def demo():
+    # Create acct
+    addr, pk = get_accounts()[0]
+    print("Using {}".format(addr))
+
+    # Read in the json contract description and create a Contract object
+    c = get_contract_from_json() 
+
+    try:
+        # Create app
+        app_id = create_app(addr, pk)
+        print("Created App with id: {}".format(app_id))
+
+        signer = AccountTransactionSigner(pk)
+
+        # set the fee to 2x min fee, this allows the inner app call to proceed even though the app address is not funded
+        sp = client.suggested_params()
+        # Create atc to handle method calling for us
+        atc = AtomicTransactionComposer()
+        # add a method call to "call" method, pass the second app id so we can dispatch a call
+        atc.add_method_call(app_id, get_method(c, "acct_param"), addr, sp, signer, method_args=[addr])
+        atc.add_method_call(app_id, get_method(c, "pad"), addr, sp, signer)
+        # run the transaction and wait for the restuls
+        result = atc.execute(client, 4)
+
+        #Print out the result
+        print("""Result of inner app call: 
+        {}""".format(result.abi_results[0].return_value))
+    finally:
+        delete_app(app_id, addr, pk)
+
+def delete_app(app_id, addr, pk):
+    # Get suggested params from network
+    sp = client.suggested_params()
+
+    # Create the transaction
+    txn = ApplicationDeleteTxn( addr, sp, app_id )
+
+    #sign it
+    signed = txn.sign(pk)
+
+    # Ship it
+    txid = client.send_transaction(signed)
+
+    # Wait for the result so we can return the app id
+    return wait_for_confirmation(client, txid, 4)
+
+
+def create_app(addr, pk):
+    # Get suggested params from network
+    sp = client.suggested_params()
+
+    # Read in approval teal source && compile
+    app_result = client.compile(get_approval())
+    app_bytes = base64.b64decode(app_result["result"])
+
+    # Read in clear teal source && compile
+    clear_result = client.compile(get_clear())
+    clear_bytes = base64.b64decode(clear_result["result"])
+
+    # We dont need no stinkin storage
+    schema = StateSchema(0, 0)
+
+    # Create the transaction
+    create_txn = ApplicationCreateTxn(
+        addr, sp, 0, app_bytes, clear_bytes, schema, schema
+    )
+
+    # Sign it
+    signed_txn = create_txn.sign(pk)
+
+    # Ship it
+    txid = client.send_transaction(signed_txn)
+
+    # Wait for the result so we can return the app id
+    result = wait_for_confirmation(client, txid, 4)
+
+    return result["application-index"]
+
+def get_contract_from_json():
+    with open("contract.json") as f:
+        js = f.read()
+
+    return Contract.from_json(js)
+
+if __name__ == "__main__":
+    demo()
+
