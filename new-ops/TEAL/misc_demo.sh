e4cf04a2fb24c862ea18a5cdf5a21fd31681652e
nullun <git@nullun.com>
2022-01-25 09:52:12 +0000

    Change APP_ID to MISC_APP_ID to prevent confusion.

--- new-ops/TEAL/misc_demo.sh
+++ new-ops/TEAL/misc_demo.sh
@@ -28,22 +28,22 @@ else
 	echo $INT1_ID > .INT1_ID
 fi;
 
-# If an .APP_ID file exists the ID from there will be used, otherwise it will
+# If an .MISC_APP_ID file exists the ID from there will be used, otherwise it will
 # deploy a new application and store the ID in it.
-if [ -f ".APP_ID"  ]; then
-	APP_ID=$(cat .APP_ID)
+if [ -f ".MISC_APP_ID"  ]; then
+	MISC_APP_ID=$(cat .MISC_APP_ID)
 else
-	APP_ID=$(${GOAL} app create --creator ${CREATOR} --approval-prog misc_demo.teal --clear-prog clear.teal --global-byteslices 0 --global-ints 0 --local-byteslices 0 --local-ints 0 | grep 'Created app with app index' | awk '{print $6}' | tr -d '\r')
-	echo $APP_ID > .APP_ID
+	MISC_APP_ID=$(${GOAL} app create --creator ${CREATOR} --approval-prog misc_demo.teal --clear-prog clear.teal --global-byteslices 0 --global-ints 0 --local-byteslices 0 --local-ints 0 | grep 'Created app with app index' | awk '{print $6}' | tr -d '\r')
+	echo $MISC_APP_ID > .MISC_APP_ID
 fi;
 
 # Application calls
 
 # Call the method "opcode_budget_demo()uint64" and the return value will be the
 # opcode budget by the end of the script.
-${GOAL} app method --app-id ${APP_ID} --method "opcode_budget_demo()uint64" -f ${USER1} --fee 2000 --foreign-app ${INT1_ID}
+${GOAL} app method --app-id ${MISC_APP_ID} --method "opcode_budget_demo()uint64" -f ${USER1} --fee 2000 --foreign-app ${INT1_ID}
 
 # Call the method "bsqrt_demo(byte[])byte" with a byte-array and the return
 # value will be the result after evaluating it with the opcode `bsqrt`.
-${GOAL} app method --app-id ${APP_ID} --method "bsqrt_demo(byte[])byte" -f ${USER1} --arg '[128]'
+${GOAL} app method --app-id ${MISC_APP_ID} --method "bsqrt_demo(byte[])byte" -f ${USER1} --arg '[128]'
 
